a15bfab1160cb494341327c23c6d73a2dbe3fe1d
Alex Volt <foryouco@yandex.ru>
2024-10-23 11:39:14 +0300

    update

---
+++ rent_sender/src/test/java/com/example/rent_sender/RentSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rent_sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
